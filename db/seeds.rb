e7a81496625d2c8ff5e00f1844f3b18dad3c5768
Diana Pinon <dianapinonb@gmail.com>
2020-11-12 14:26:58 -0700

    Added image_url to cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url_serialized = open(url).read
+ingredients_json = JSON.parse(url_serialized)
+ingredients = ingredients_json['drinks']
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
